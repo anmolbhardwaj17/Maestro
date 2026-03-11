2a2af44c21789dccf80568a8d386bd31fab81350
Anmol <anmol.bhardwaj@siemba.io>
2026-03-12 04:30:50 +0530

    Use Analytics React component instead of inject

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { inject } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 import './styles.css';
 import App from './App';
-
-inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
